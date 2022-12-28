aa46f0bcbe5be648d3061dc012ff86e8cf19a8d6
Juan Sáez García <jsaez@odec.es>
2022-12-28 10:49:59 +0100

    useValue

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+const API_KEY_DEV = 'DEV-A7H3OP6';
+const API_KEY_PROD = 'PRO-DDH9ASD';
+
 @Module({
   imports: [UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY_DEV,
+    },
+  ],
 })
 export class AppModule {}
