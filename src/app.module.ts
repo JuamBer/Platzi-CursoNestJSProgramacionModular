bc19c0fda36314dc701513fafabfb945891f36cc
Juan Sáez García <jsaez@odec.es>
2022-12-28 15:25:23 +0100

    Validación de esquemas en .envs con Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
 import { environments } from 'environments/environment';
 import config from '../environments/config';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
@@ -15,6 +16,11 @@ import config from '../environments/config';
         environments[process.env.NODE_ENV] || './environments/.dev.env',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
     HttpModule,
     UsersModule,
